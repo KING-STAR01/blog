701649b4698f20a3d71932927990c381b788c4d2
c-prasanth <prasanth@stockone.com>
2023-01-07 16:06:03 +0530

    updated comments

--- blog/admin.py
+++ blog/admin.py
@@ -4,6 +4,19 @@ from .models import Article, UserProfile, Comments
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ['title', 'author', 'created_on']
 
+<<<<<<< HEAD
 admin.site.register(Article,ArticleAdmin)
 admin.site.register(UserProfile)
 admin.site.register(Comments)
+=======
+class CommentAdmin(admin.ModelAdmin):
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(active = True)
+
+
+admin.site.register(Article,ArticleAdmin)
+admin.site.register(UserProfile)
+admin.site.register(Comments, CommentAdmin)
+>>>>>>> 830b7db (blog website)
